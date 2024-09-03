33e638849d6d41c6319800e5c4820bdd45ebc7d4
Tania <taniagoia11@gmail.com>
2024-09-03 13:53:22 +0000

    aaaa

---
+++ benchmarksClass/__init__.py
@@ -0,0 +1,19 @@
+# from ._distribution import Distribution
+from ._benchmarks import Benchmarks
+# from ._beta import Beta
+# from ._cauchy import Cauchy
+# from ._cmrf import CMRF
+# from ._gamma import Gamma
+# from ._modifiedhalfnormal import ModifiedHalfNormal
+# from ._gaussian import Gaussian, JointGaussianSqrtPrec
+# from ._gmrf import GMRF
+# from ._inverse_gamma import InverseGamma
+# from ._lmrf import LMRF
+# from ._laplace import Laplace
+# from ._smoothed_laplace import SmoothedLaplace
+# from ._lognormal import Lognormal
+# from ._normal import Normal
+# from ._posterior import Posterior
+# from ._uniform import Uniform
+# from ._custom import UserDefinedDistribution, DistributionGallery
+# from ._joint_distribution import JointDistribution, _StackedJointDistribution, MultipleLikelihoodPosterior
