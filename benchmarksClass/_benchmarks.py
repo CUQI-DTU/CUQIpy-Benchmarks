c90961ebce5b0bc738c8e3f85c1e61c0d270216b
Tania <taniagoia11@gmail.com>
2024-09-04 10:34:29 +0000

    simplest bip class

--- benchmarksClass/_benchmarks.py
+++ benchmarksClass/_benchmarks.py
@@ -1,17 +1,16 @@
 import numpy as np
 from cuqi.problem import BayesianProblem
-from cuqi.distribution import Distribution, JointDistribution, UserDefinedDistribution
+from cuqi.distribution import Distribution, JointDistribution, UserDefinedDistribution, Gaussian
  
 class Benchmarks(UserDefinedDistribution):
-    def __init__(self,model_type,dim, log_pdf = None,grad_logpdf = None,prior_distribution = None,forward_operator = None, additive_noise = None,model = None,data = None, **kwargs):
+    def __init__(self,model_type,dim, log_pdf = None,grad_logpdf = None,prior_distribution = None,model = None, noise=None, forward_operator = None,data = None, **kwargs):
             # Init from abstract distribution class
             if model_type == "distribution":
                 super().__init__(dim = dim,logpdf_func = log_pdf, gradient_func = grad_logpdf, **kwargs)
             
             elif model_type == "bayesian" :
                 x = prior_distribution
-                A = forward_operator
-                y = model
+                y = Gaussian(forward_operator.forward(x),noise)
                 BP = BayesianProblem(y, x)
                 BP.set_data(y = data) 
-                super().__init__(dim = dim,logpdf_func = BP.posterior.logpdf, gradient_func = BP.posterior.gradient, **kwargs)
+                super().__init__(dim = dim, logpdf_func = BP.posterior.logpdf, gradient_func = BP.posterior.gradient, **kwargs)
--- benchmarksClass/_simplestbip.py
+++ benchmarksClass/_simplestbip.py
@@ -1,13 +1,13 @@
 import numpy as np
 from cuqi.distribution import Distribution, DistributionGallery, Gaussian, JointDistribution, UserDefinedDistribution
 from cuqi.model import LinearModel
+from cuqi.problem import BayesianProblem
 from ._benchmarks import Benchmarks
 
 class SimplestBip(Benchmarks):
-  def __init__(self,dim = 2, matrix = np.array([[1.0, 1.0]]),noise = 0.1,data = [1,5,1.5],**kwargs):
+  def __init__(self,dim = 2, matrix = np.array([[1.0, 1.0]]),noise = 0.1,data = 3,**kwargs):
     x = Gaussian(np.zeros(dim), 2.5)
     A = LinearModel(matrix)
-    model = Gaussian(A.forward(x),noise)
-    super().__init__(model_type = "bayesian",dim = 2,prior_distribution = x,forward_operator =  A, additive_noise = noise,model = model,data = data, **kwargs)
+    super().__init__(model_type = "bayesian", dim = 2, prior_distribution = x, noise = noise, forward_operator = A, data = data,**kwargs)
   
   
