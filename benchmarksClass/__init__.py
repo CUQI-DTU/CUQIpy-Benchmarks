45423204a1aa81a70ed1c307de78e694eef526fc
Tania <taniagoia11@gmail.com>
2024-09-04 08:34:26 +0000

    added sixmodal class

--- benchmarksClass/__init__.py
+++ benchmarksClass/__init__.py
@@ -2,6 +2,7 @@
 from ._benchmarks import Benchmarks
 from ._donut import Donut 
 from ._banana import Banana
+from ._sixmodal import Sixmodal
 # from ._beta import Beta
 # from ._cauchy import Cauchy
 # from ._cmrf import CMRF
